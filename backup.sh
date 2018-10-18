75849049c255f24286ddae8fcb0b5ae05b1080e1
yenho <yenho.tseng@gmail.com>
2018-10-18 15:34:55 +0800

    Log files move to /tmp folder

--- backup.sh
+++ backup.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-# remove the previous test logs -- stat_data.txt and log_for_health_check.txt
 if [ -a stat_data.txt ]
-then rm stat_data.txt
+then rm /tmp/stat_data.txt
   echo removed old log file
 fi
 
-if [ -a log_for_health_check.txt ]
-then rm log_for_health_check.txt
+if [ -a /tmp/log_for_health_check.txt ]
+then rm /tmp/log_for_health_check.txt
   echo removed old log file
 fi
 
 echo sampling......
-for i in {1..3000}
+for i in {1..10000}
 #this script runs on bud for about 30 secs, it means about 60 samples/sec, 1 sample/waveform
  do
-   cat /sys/bus/iio/devices/iio\:device0/sampling_frequency >> log_for_health_check.txt
+   cat /sys/bus/iio/devices/iio\:device0/sampling_frequency >> /tmp/log_for_health_check.txt
    sleep 0.00
  done
 #sed 's/.*debug=\(.*\),\(.*\)/\1 \2/' log_for_health_check.txt | python pllstats.py
 # sed 's/.*debug=\(.*\),\(.*\)/\1 \2/' log_for_health_check.txt 
 #sed 's/.*state=\(.*[0-9]\)/\1/' | sed 's/ *halt.*$//g' log_for_health_check.txt
 #sed 's/ *halt.*halt.*$//g' | sed 's/.*state=\(.*[0-9]\)/\1/' log_for_health_check.txt
-cat log_for_health_check.txt | sed 's/ *halt.*$//g' | sed 's/.*state=\(.*[0-9]\)/\1/'  > stat_data.txt
+cat /tmp/log_for_health_check.txt | sed 's/ *halt.*$//g' | sed 's/.*state=\(.*[0-9]\)/\1/'  > /tmp/stat_data.txt
 python check_n_backup.py
--- check_n_backup.py
+++ check_n_backup.py
@@ -5,7 +5,7 @@ from subprocess import call
 
 dataStat = []
 
-f = open('stat_data.txt')
+f = open('/tmp/stat_data.txt')
 for line in f:
   dataStat.append(int(line))
 f.close()
@@ -18,10 +18,11 @@ min_val = min(dataStat)
 # print 'min : ' + str(min_val)
 
 date_str = time.strftime("_%Y-%m-%d_%H_%M_%S", time.localtime())
-cp_filename = 'update_date' + date_str + '.txt'
+cp_filename = '/media/sdcard/update_date' + date_str + '.txt'
+# cp_filename = '/tmp/update_date' + date_str + '.txt'
 # print cp_filename
 
-cp_cmd = 'cp log_for_health_check.txt ' + cp_filename
+cp_cmd = 'cp /tmp/log_for_health_check.txt ' + cp_filename
 # print cp_cmd
 # cp_filename = "update_%{time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())}.txt"
 #print time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())
@@ -32,6 +33,6 @@ if max_val == 2:
   if max_val > min_val:
     call(cp_cmd, shell=True)
   else:
-    call("rm log_for_health_check.txt", shell=True)
+    call("rm /tmp/log_for_health_check.txt", shell=True)
 else:
     call(cp_cmd, shell=True)
