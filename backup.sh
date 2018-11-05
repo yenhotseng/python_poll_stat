bd6a50954696f2bc22e3e80d5a7d2b070e0241a2
yenhotseng <yenho@verdigris.co>
2018-11-05 15:05:01 +0800

    Update backup.sh to filter gdaq restart

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 if [ -a /tmp/stat_data.txt ]
 then rm /tmp/stat_data.txt
-  echo removed old log file
+  echo removed stat log file
 fi
 
 if [ -a /tmp/log_for_health_check.txt ]
@@ -14,4 +14,12 @@ echo sampling......
 
 /root/read_f 270
 cat /tmp/log_for_health_check.txt | sed 's/ *halt.*$//g' | sed 's/.*state=\(.*[0-9]\)/\1/'  > /tmp/stat_data.txt
+
+cat /tmp/log_for_health_check.txt | grep 'halt=1,1'  > /tmp/halt.txt
+
 python check_n_backup.py
+
+if [ -a /tmp/halt.txt ]
+then rm /tmp/halt.txt
+  echo removed halt log file
+fi
--- check_n_backup.py
+++ check_n_backup.py
@@ -15,25 +15,25 @@ max_val = max(dataStat)
 # print 'max : ' + str(max_val)
 
 min_val = min(dataStat)
-# print 'min : ' + str(min_val)
 
 date_str = time.strftime("_%Y-%m-%d_%H_%M_%S", time.localtime())
 cp_filename = '/media/sdcard/update_date' + date_str + '.txt'
-# cp_filename = '/tmp/update_date' + date_str + '.txt'
-# print cp_filename
 
 cp_cmd = 'cp /tmp/log_for_health_check.txt ' + cp_filename
 cp_cmd1 = 'uptime >> ' + cp_filename
-# print cp_cmd
-# cp_filename = "update_%{time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())}.txt"
-#print time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())
-
-# call(cp_cmd, shell=True) #debug only
+cp_cmd2 = 'mv ' + cp_filename + ' /media/sdcard/archived'
 
 if max_val == 2:
   if max_val > min_val:
     call(cp_cmd, shell=True)
     call(cp_cmd1, shell=True)
+    f = open('/tmp/halt.txt')
+    for line in f:
+        dataStat.append(int(line))
+    f.close()
+    max_val = max(dataStat)
+    if max_val == 2:
+        call(cp_cmd2, shell=True)
   else:
     call("rm /tmp/log_for_health_check.txt", shell=True)
 else:
